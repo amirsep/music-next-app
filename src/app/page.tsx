58ca9bd55971492f45237f3ddc254eb8f75682cf
amirsep <amir.sep19@gmail.com>
2024-03-05 19:56:48 +0530

    update

---
+++ src/app/page.tsx
@@ -0,0 +1,23 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
+import Footer from "@/components/Footer";
+import HeroSection from "@/components/HeroSection";
+import Instructors from "@/components/Instructors";
+import TestimonialCard from "@/components/TestimonialCard";
+import UpcomingWebinars from "@/components/UpcomingWebinars";
+import WhyChooseUs from "@/components/WhyChooseUs";
+
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02] ">
+      <HeroSection />
+      <FeaturedCourses />
+      <WhyChooseUs />
+      <TestimonialCard />
+      <UpcomingWebinars />
+      <Instructors />
+      <Footer />
+    </main>
+  );
+}
